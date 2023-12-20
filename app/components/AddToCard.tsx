c624c3594573399e2110f837d03ca7b571278bf0
jdjoaquin <joaquinjdj78@gmai.com>
2023-12-20 14:56:19 +0800

    first commit

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={() => console.log('click')}>Click Me</button>
+  )
+}
+
+export default AddToCard
---
+++ app/users/page.tsx
@@ -0,0 +1,9 @@
+const UserPage = () => {
+    return (
+        <div>
+            <p>Users Page</p>
+        </div>
+    )
+};
+
+export default UserPage
